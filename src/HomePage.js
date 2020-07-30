5bc42df13b6acad8f472571c45af74f846b748f0
linneat <51674082+linneat@users.noreply.github.com>
2020-07-30 11:43:14 +0200

    show all articles

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,16 +3,63 @@ import { Link } from "react-router-dom";
 import ArticleNew from "./ArticleNew";
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: undefined,
+      articles: undefined,
+    };
+  }
+
+  componentDidMount() {
+    let url = "http://localhost:3000/articles.json";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          this.setState({
+            articles: data,
+          });
+        },
+        (error) => {
+          this.setState({
+            error: error.message,
+          });
+        }
+      );
+  }
   render() {
-    return (
-      <div className="appWrap">
-        <div className="homePageWrap">
-          <h1 className="heading">Posted Articles</h1>
-          <Link to={"/articlenew"} className="buttonHomePage">
-            <button className="buttonClass">Write new article</button>
-          </Link>
+    if (this.state.error) {
+      return <div>an error occured: {this.state.error}</div>;
+    } else if (this.state.articles === undefined) {
+      return (
+        <div>
+          <div>loading articles...</div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="appWrap">
+          <div className="homePageWrap">
+            <h1 className="heading">Posted Articles</h1>
+            <ul>
+              {this.state.articles.map((item, index) => {
+                return (
+                  <li key={index}>
+                    {item.title} {item.text}
+                  </li>
+                );
+              })}
+            </ul>
+
+            <Link to={"/articlenew"} className="buttonHomePage">
+              <button className="buttonClass">Write new article</button>
+            </Link>
+          </div>
+        </div>
+      );
+    }
   }
 }
