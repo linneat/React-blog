78049df5c843d18fcbb38c49db72ca3071e96ffc
linneat <51674082+linneat@users.noreply.github.com>
2020-07-31 20:10:58 +0200

    Updates

---
+++ precache-manifest.58a1e67150569d0062a838be2928ae27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0431e1841a4c08dc9e2484f03eed6942",
+    "url": "/React-blog/index.html"
+  },
+  {
+    "revision": "08c2b47483858b04cc70",
+    "url": "/React-blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb53e45f9ab0bbe988db",
+    "url": "/React-blog/static/js/2.db3a6d9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-blog/static/js/2.db3a6d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c2b47483858b04cc70",
+    "url": "/React-blog/static/js/main.382e4699.chunk.js"
+  },
+  {
+    "revision": "d3e5d0df4d5d854d7ad8",
+    "url": "/React-blog/static/js/runtime-main.539a9c41.js"
+  }
+]);
