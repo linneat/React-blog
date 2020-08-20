355547e1d09cf7021094edcbfb5d93e01b040527
linneat <51674082+linneat@users.noreply.github.com>
2020-08-20 07:37:21 +0200

    Updates

---
+++ precache-manifest.3a539e1e00aa9ad656b2366771581c77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a4bf3d4080da5bee054d5c89955b35",
+    "url": "/React-blog/index.html"
+  },
+  {
+    "revision": "1767a96b1cb62eddcdc5",
+    "url": "/React-blog/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8c985c76dd18f208ed44",
+    "url": "/React-blog/static/css/main.81a1553e.chunk.css"
+  },
+  {
+    "revision": "1767a96b1cb62eddcdc5",
+    "url": "/React-blog/static/js/2.319badb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-blog/static/js/2.319badb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c985c76dd18f208ed44",
+    "url": "/React-blog/static/js/main.4461b412.chunk.js"
+  },
+  {
+    "revision": "d3e5d0df4d5d854d7ad8",
+    "url": "/React-blog/static/js/runtime-main.539a9c41.js"
+  },
+  {
+    "revision": "35d42ffc43060327ed627a76462cd7b7",
+    "url": "/React-blog/static/media/newspaper.35d42ffc.jpg"
+  }
+]);
